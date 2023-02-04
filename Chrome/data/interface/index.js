7e222756d36aec38acc169c405d77d322a98d096
Hemanta Gayen <69314427+hemucode@users.noreply.github.com>
2023-02-04 14:20:53 +0530

    gh

--- Chrome/data/interface/index.js
+++ Chrome/data/interface/index.js
@@ -1,64 +1,35 @@
-async function init() {
-  return Promise.all([translate(), hydrate()]);
-}
-
-function translate() {
-  return new Promise((resolve) => {
-    const elements = document.querySelectorAll("[data-message]");
-    for (const element of elements) {
-      const key = element.dataset.message;
-      const message = chrome.i18n.getMessage(key);
-      if (message) {
-        element.textContent = message;
-      } else {
-        console.error("Missing chrome.i18n message:", key);
-      }
-    }
-    resolve();
+var time = "";
+window.onload = function(){ 
+  chrome.runtime.sendMessage({
+    getTime: ""
+  }, function () {
+    console.log('success of get Time');
   });
-}
-
-/**
- * @returns Promise
- */
-async function hydrate() {
-   var a = new Promise(function(resolve, reject){
-        chrome.storage.sync.get({"enabled": true}, function(options){
-            resolve(options.enabled);
-        })
+  document.querySelector("#time").addEventListener("change", function()  {
+    time = document.querySelector('#time').value;
+    console.log(time);
+    if (time === 'notset')
+      time = 0;
+    chrome.runtime.sendMessage({
+      clearTime: time
+    }, function () {
+      console.log('msg sent');
     });
-
-  const enabled = await a;
-
-  // Hydrate Logo
-  const $logo = document.querySelector(".logo");
-  $logo.style.filter = enabled ? "grayscale(0)" : "grayscale(100%)";
-  $logo.style.opacity = enabled ? "1" : "0.7";
-
-
-  const $checkboxLabel = document.querySelector("[data-message=enabled]");
-  $checkboxLabel.textContent = chrome.i18n.getMessage(
-    enabled ? "enabled" : "disabled"
-  );
-
-  // Hydrate Checkbox Label
-  const $enabledCheckbox = document.querySelector("input[name=enabled]");
-  $enabledCheckbox.checked = enabled;
-  $enabledCheckbox.addEventListener("change", async (event) => {
-    const enabled = event.currentTarget.checked;
-
-    // Persist
-    await chrome.storage.sync.set({ enabled });
-
-    // Update Checkbox Label
-    $checkboxLabel.textContent = chrome.i18n.getMessage(
-      enabled ? "enabled" : "disabled"
-    );
-
-    // Update Logo
-    $logo.style.filter = enabled ? "grayscale(0)" : "grayscale(100%)";
-    $logo.style.opacity = enabled ? "1" : "0.7";
   });
+  document.querySelector("#okBtn").addEventListener("click", function () {
+    window.close();
+  });
+
+
 }
 
-init();
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.clearTime !== null && request.clearTime !== undefined) {
+    console.log('bg.js message received :' + request.clearTime);
+    time = request.clearTime;
+    if (time === 0)
+      time = 'notset';
+    document.querySelector("#time").value=time;
+  }
+});
+
